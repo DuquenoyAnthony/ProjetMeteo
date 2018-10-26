27f05a2224141f27cfb27113ce4ac79e43f5da52
DuquenoyAnthony <44491866+DuquenoyAnthony@users.noreply.github.com>
2018-10-26 09:19:13 +0200

    Add files via upload

---
+++ Projet météo/js/script.js
@@ -0,0 +1,15 @@
+//var nomVille = $("#temp form").get('rechercheVille');
+function recherche()
+{
+	var nomVille = document.getElementById("rechercheVille").value;
+	$.ajax({
+		url: "https://api.openweathermap.org/data/2.5/weather?q="+ nomVille +"&units=metric&APPID=032aa7acbc4a4d21a85dc6638409262b",
+		success: function( result ) {
+		console.log(result);
+		$( "#ex1" ).html( "Il fait <strong>" + result.main.temp + "</strong> degrés à " + result.name );
+		},
+		error: function(result){
+			$( "#ex1" ).html( "Cette ville n'existe pas" );
+		}
+	});
+}
