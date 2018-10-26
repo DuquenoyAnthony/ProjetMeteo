d1cd47ddc8b14b66fc970534162c7ca6eb99bc9f
peccccccccccc <44492199+peccccccccccc@users.noreply.github.com>
2018-10-26 16:59:34 +0200

    Fonction recherche forecast

--- Projet météo/js/script.js
+++ Projet météo/js/script.js
@@ -3,10 +3,12 @@ function recherche()
 {
 	var nomVille = document.getElementById("rechercheVille").value;
 	$.ajax({
-		url: "https://api.openweathermap.org/data/2.5/weather?q="+ nomVille +"&units=metric&APPID=032aa7acbc4a4d21a85dc6638409262b",
+		url: "http://api.openweathermap.org/data/2.5/forecast?q="+nomVille+"&units=metric&APPID=032aa7acbc4a4d21a85dc6638409262b",
 		success: function( result ) {
 		console.log(result);
-		$( "#ex1" ).html( "Il fait <strong>" + result.main.temp + "</strong> degrés à " + result.name );
+		$( "#ex1" ).html( "Il fait <strong>" + result.list[0].main.temp+ "</strong> degrés à " + result.city.name +". L'humidité est de "+ result.list[0].main.humidity);
+		$( "#ex1" ).append( " Il fait <strong>" + result.list[1].main.temp );
+		$( "#ex1" ).append( " Il fait <strong>" + result.list[2].main.temp );
 		},
 		error: function(result){
 			$( "#ex1" ).html( "Cette ville n'existe pas" );
