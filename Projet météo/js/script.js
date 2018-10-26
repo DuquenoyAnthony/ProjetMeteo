6e581827c8e854fcf7d5002c493c659e0a83c447
DuquenoyAnthony <duquenoy.anthony@outlook.com>
2018-10-26 15:50:05 +0200

    Ajout de la date courrante en js

--- Projet météo/js/script.js
+++ Projet météo/js/script.js
@@ -1,4 +1,15 @@
 //var nomVille = $("#temp form").get('rechercheVille');
+var date = new Date();
+var tab_jour=new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+var tab_mois=new Array("Décembre","Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre");
+var annee   = date.getFullYear();
+var mois    = date.getMonth() + 1;
+var jour    = date.getDate();
+var heure   = date.getHours();
+var minute  = date.getMinutes();
+var seconde = date.getSeconds();
+
+
 function recherche()
 {
 	var nomVille = document.getElementById("rechercheVille").value;
@@ -6,10 +17,16 @@ function recherche()
 		url: "https://api.openweathermap.org/data/2.5/weather?q="+ nomVille +"&units=metric&APPID=032aa7acbc4a4d21a85dc6638409262b",
 		success: function( result ) {
 		console.log(result);
-		$( "#ex1" ).html( "Il fait <strong>" + result.main.temp + "</strong> degrés à " + result.name );
+		var date = new Date();
+		var tab_jour=new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+		$( "#ex1" ).html( "Il fait <strong>" + result.main.temp + "</strong> degrés à " + result.name + " le "+ tab_jour[date.getDay()]);
 		},
 		error: function(result){
 			$( "#ex1" ).html( "Cette ville n'existe pas" );
 		}
 	});
 }
+$('document').ready(function(){
+	$(".day").html(tab_jour[date.getDay()]);
+	$(".date").html(jour + " " + tab_mois[mois]);
+});
