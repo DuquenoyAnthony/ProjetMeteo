2676e10c4d657ba461497844b4e2208dc82c0355
peccccccccccc <44492199+peccccccccccc@users.noreply.github.com>
2018-10-26 14:34:23 +0200

    Update Projet météo/js/script.js

--- Projet météo/js/script.js
+++ Projet météo/js/script.js
@@ -3,7 +3,7 @@ function recherche()
 {
 	var nomVille = document.getElementById("rechercheVille").value;
 	$.ajax({
-		url: "https://api.openweathermap.org/data/2.5/weather?q="+ nomVille +"&units=metric&APPID=032aa7acbc4a4d21a85dc6638409262b",
+		url: "https://api.openweathermap.org/data/2.5/forecast?q="+ nomVille +"&units=metric&APPID=032aa7acbc4a4d21a85dc6638409262b",
 		success: function( result ) {
 		console.log(result);
 		$( "#ex1" ).html( "Il fait <strong>" + result.main.temp + "</strong> degrés à " + result.name );
